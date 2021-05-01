f6e2d308f67b645130b54a531b7a6382f600b2b7
Ivan Gagarinov <dzencot@gmail.com>
2021-05-01 12:26:21 +0500

    add web tests

--- web/src/app/app.component.spec.ts
+++ web/src/app/app.component.spec.ts
@@ -1,35 +1,17 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { render, screen, fireEvent } from '@testing-library/angular'
+import { APP_BASE_HREF } from '@angular/common';
+
+import { AppComponent } from './app.component'
+import { AppModule } from './app.module';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+  test('just init', async () => {
+    await render(AppComponent, {
+      imports: [ AppModule ],
+      providers: [ { provide: APP_BASE_HREF, useValue : '/' } ],
+      excludeComponentDeclaration: true,
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('app app is running!');
+    expect(screen.getByText('First Name'));
   });
 });
